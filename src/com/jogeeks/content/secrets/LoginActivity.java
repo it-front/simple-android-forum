2b59d59930136e698335198e1b4123bb50e63e24
rmarji <raed@jogeeks.com>
2014-02-19 21:00:23 +0200

    annotations

--- src/com/jogeeks/content/secrets/LoginActivity.java
+++ src/com/jogeeks/content/secrets/LoginActivity.java
@@ -1,11 +1,15 @@
 package com.jogeeks.content.secrets;
 
+import org.androidannotations.annotations.AfterViews;
+import org.androidannotations.annotations.Background;
 import org.androidannotations.annotations.Click;
 import org.androidannotations.annotations.EActivity;
 import org.androidannotations.annotations.OptionsMenu;
 import org.androidannotations.annotations.ViewById;
+import org.androidannotations.annotations.rest.RestService;
 
 import android.app.Activity;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 
@@ -15,18 +19,31 @@ import com.jogeeks.wordpress.WPLogin;
 @OptionsMenu(R.menu.login)
 public class LoginActivity extends Activity {
 
+	@RestService
+	MyRestClient myRestClient;
+
 	@ViewById(R.id.username)
 	EditText usernameInput;
 
 	@ViewById(R.id.password)
 	EditText passwordInput;
-	
+
 	@ViewById(R.id.login)
 	Button loginButton;
-	
+
 	@Click
-	void login(){
+	void login() {
 		new WPLogin(this, "firas", "123123", SecretsActivity.class);
 	}
-	
+
+	@AfterViews
+	void afterViews() {
+		getPostsAsync("", "");
+	}
+
+	@Background
+	void getPostsAsync(String searchString, String userId) {
+		Log.d("REST", myRestClient.getRecentPosts().toString());
+	}
+
 }
