0fa973b04e277e1cf4c811fe3166e227a08bb199
rmarji <raed@jogeeks.com>
2014-02-19 11:23:55 +0200

    add android annotations and edited the login activity to reflect the code diet that happened because of it.

--- src/com/jogeeks/content/secrets/LoginActivity.java
+++ src/com/jogeeks/content/secrets/LoginActivity.java
@@ -1,52 +1,32 @@
 package com.jogeeks.content.secrets;
 
+import org.androidannotations.annotations.Click;
+import org.androidannotations.annotations.EActivity;
+import org.androidannotations.annotations.OptionsMenu;
+import org.androidannotations.annotations.ViewById;
+
 import android.app.Activity;
-import android.os.Bundle;
-import android.view.Menu;
-import android.view.View;
-import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
 import com.jogeeks.wordpress.WPLogin;
 
-public class LoginActivity extends Activity implements OnClickListener {
+@EActivity(R.layout.activity_login)
+@OptionsMenu(R.menu.login)
+public class LoginActivity extends Activity {
 
-	private EditText usernameInput;
-	private EditText passwordInput;
-	private Button loginButton;
+	@ViewById(R.id.username)
+	EditText usernameInput;
 
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		setContentView(R.layout.activity_login);
-
-		// References
-		usernameInput = (EditText) findViewById(R.id.username);
-		passwordInput = (EditText) findViewById(R.id.password);
-		loginButton = (Button) findViewById(R.id.login);
-
-		// OnClickListener
-		loginButton.setOnClickListener(this);
+	@ViewById(R.id.password)
+	EditText passwordInput;
+	
+	@ViewById(R.id.login)
+	Button loginButton;
+	
+	@Click
+	void login(){
+		new WPLogin(this, "firas", "123123", SecretsActivity.class);
 	}
-
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.login, menu);
-		return true;
-	}
-
-	@Override
-	public void onClick(View v) {
-		switch (v.getId()) {
-		case R.id.login:
-			new WPLogin(this, "firas", "123123", SecretsActivity.class);
-			break;
-
-		default:
-			break;
-		}
-	}
-
+	
 }
